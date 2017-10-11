c07c2f022fb9e8aae0967a63b9a286e5f29fd4f6
Ben <whoyumengmeng@gmail.com>
2017-10-11 23:13:16 +0800

    init

---
+++ src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -0,0 +1,26 @@
+package spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class<?>[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class<?>[] {WebConfig.class} ;
+	}
+
+	// 将dispatcherServlet映射到“/”
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" };
+	}
+
+}
