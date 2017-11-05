f894faeeeed817ea1b375775e24b60ea5d156301
Ben <Ben@Ben-PC>
2017-11-05 21:40:37 +0800

    test spittle success

--- src/test/java/spittr/web/SpittleControllerTest.java
+++ src/test/java/spittr/web/SpittleControllerTest.java
@@ -1,10 +1,13 @@
 package spittr.web;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.taglibs.standard.tag.common.xml.WhenTag;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -49,6 +52,21 @@ public class SpittleControllerTest {
 				.andExpect(MockMvcResultMatchers.model().attributeExists("spittleList")).andExpect(MockMvcResultMatchers
 						.model().attribute("spittleList", Matchers.hasItems(expectedSpittles.toArray())));
 
+	}
+
+	@Test
+	public void testSpittle() throws Exception {
+		Spittle expectedSpittle = new Spittle("hello", new Date());
+		SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
+		Mockito.when(mockRepository.findOne(12345)).thenReturn(expectedSpittle);
+
+		SpittleController controller = new SpittleController(mockRepository);
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		// 通过路径请求资源
+		mockMvc.perform(MockMvcRequestBuilders.get("/spittles/12345"))
+				.andExpect(MockMvcResultMatchers.view().name("spittle"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("spittle"))
+				.andExpect(MockMvcResultMatchers.model().attribute("spittle", expectedSpittle));
 	}
 
 	private List<Spittle> creatSpittleList(int count) {
